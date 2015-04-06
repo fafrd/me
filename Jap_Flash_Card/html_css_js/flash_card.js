8c30bc38316b50809ff3dff7935ee7d7201ffb33
lambui <spot.lam@gmail.com>
2015-04-05 23:28:03 -0700

    add index tracker and solution display

--- Jap_Flash_Card/html_css_js/flash_card.js
+++ Jap_Flash_Card/html_css_js/flash_card.js
@@ -21,7 +21,7 @@ function findIndex(lessonName)
 function show()
 {
 	var i = findIndex(lessonName);
-	document.getElementById("question").innerHTML = library[i][index].eng;
+	document.getElementById("question").innerHTML = index + "/" + (library[i].length-1) + ": " + library[i][index].eng;
 }
 
 function execute()
@@ -29,13 +29,13 @@ function execute()
 	var i = findIndex(lessonName);
 	input = document.getElementById("inputBox").value;
 	if(library[i][index].romanji != input)
-		document.getElementById("result").innerHTML = "wrong!";
+		document.getElementById("result").innerHTML = "wrong! Right answer is: " + library[i][index].romanji;
 	else
 		document.getElementById("result").innerHTML = "right!";
 	index++;
 	if(index >= library[i].length) index = 1;
 	window.setTimeout('show()',500);
-	window.setTimeout('document.getElementById("result").innerHTML = "";',500);
+	window.setTimeout('document.getElementById("result").innerHTML = "";',2000);
 	window.setTimeout('document.getElementById("inputBox").value = ""',500);
 }
 
